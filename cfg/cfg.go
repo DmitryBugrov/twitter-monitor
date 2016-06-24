d8a7eb48c7ef5fc812c17471facfeab71acc4cdf
DmitryBugrov <dmitrybugrov@gmail.com>
2016-06-24 15:15:38 +0300

    add config module and config file

---
+++ cfg/cfg.go
@@ -0,0 +1,52 @@
+// cfg.go
+package cfg
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/DmitryBugrov/log"
+)
+
+var (
+	c        *Cfg
+	Log      *log.Log
+	Filename string
+)
+
+type Cfg struct {
+	TM struct {
+		ConsumerKey    string
+		ConsumerSecret string
+		AccessToken    string
+		AccessSecret   string
+
+		Username string //twitter user
+		Message  string //reply message
+	}
+}
+
+func (c *Cfg) Init(_Log *log.Log, _filename string) error {
+	Log = _Log
+	Log.Print(log.LogLevelTrace, "Enter to cfg.Init")
+	Filename = _filename
+	err := c.load()
+	return err
+}
+
+func (c *Cfg) load() error {
+	Log.Print(log.LogLevelTrace, "Enter to cfg.Load")
+	file, err := os.Open(Filename)
+	if err != nil {
+		Log.Print(log.LogLevelError, "Configuration file cannot be loaded: ", Filename)
+		return err
+	}
+	jsonParser := json.NewDecoder(file)
+	err = jsonParser.Decode(&c)
+	if err != nil {
+		Log.Print(log.LogLevelError, "Unable to decode config into struct", err.Error())
+		return err
+	}
+
+	return nil
+}
--- twitter-monitor.go
+++ twitter-monitor.go
@@ -3,43 +3,60 @@ package main
 
 import (
 	"net/url"
+	"os"
 	"strconv"
 
 	"github.com/ChimeraCoder/anaconda"
 	"github.com/DmitryBugrov/log"
+
+	"twitter-monitor/cfg"
 )
 
 const (
-	consumerKey    = "AerKUoivGDOTEcr7qEz3Mln0e"
-	consumerSecret = "dwsdF8YKrFlKd5gBcfO6nruVzQYKOLIP4lieS0tOoClewfRPVb"
-	accessToken    = "182329995-bCwddVYl4Z9GoeU0asFEyrjUuKfdEVtAhKOji5lg"
-	accessSecret   = "4CbxJeG7wce24ctoRW3S5jRddcILyOY9wkMMZlFYrfWpL"
+	//	consumerKey    = "AerKUoivGDOTEcr7qEz3Mln0e"
+	//	consumerSecret = "dwsdF8YKrFlKd5gBcfO6nruVzQYKOLIP4lieS0tOoClewfRPVb"
+	//	accessToken    = "182329995-bCwddVYl4Z9GoeU0asFEyrjUuKfdEVtAhKOji5lg"
+	//	accessSecret   = "4CbxJeG7wce24ctoRW3S5jRddcILyOY9wkMMZlFYrfWpL"
 
-	username = "DmitryTest1902" //twitter user
-	message  = "Hello"          //reply message
+	//	username = "DmitryTest1902" //twitter user
+	//	message  = "Hello"          //reply message
+	configFileName = "./config.json"
 )
 
 var (
 	Log    *log.Log
 	client *anaconda.TwitterApi
+	config *cfg.Cfg
 )
 
 func main() {
-	Init()
+	err := Init()
+	if !err {
+		os.Exit(1)
+	}
 	Monitor()
 
 	Log.Print(log.LogLevelTrace, "Exit")
 }
 
-func Init() {
+func Init() bool {
 	//Init logging
 	Log = new(log.Log)
 	Log.Init(log.LogLevelTrace, true, true, true)
 
+	//init config
+	config = new(cfg.Cfg)
+	err := config.Init(Log, configFileName)
+	if err != nil {
+		Log.Print(log.LogLevelError, "No configuration file loaded: ", configFileName)
+		return false
+	}
+
 	//Init twitter client
-	anaconda.SetConsumerKey(consumerKey)
-	anaconda.SetConsumerSecret(consumerSecret)
-	client = anaconda.NewTwitterApi(accessToken, accessSecret)
+	anaconda.SetConsumerKey(config.TM.ConsumerKey)
+	anaconda.SetConsumerSecret(config.TM.ConsumerSecret)
+	client = anaconda.NewTwitterApi(config.TM.AccessToken, config.TM.AccessSecret)
+	return true
 }
 
 func Monitor() {
@@ -47,15 +64,15 @@ func Monitor() {
 
 	Log.Print(log.LogLevelTrace, "Add to following")
 	v := url.Values{}
-	_, err := client.FollowUser(username)
+	_, err := client.FollowUser(config.TM.Username)
 
 	if err != nil {
-		Log.Print(log.LogLevelError, "Error add to following:", username)
+		Log.Print(log.LogLevelError, "Error add to following:", config.TM.Username)
 	}
 	v = url.Values{}
-	user, err := client.GetUsersShow(username, v)
+	user, err := client.GetUsersShow(config.TM.Username, v)
 	if err != nil {
-		Log.Print(log.LogLevelError, "Error get userid for:", username)
+		Log.Print(log.LogLevelError, "Error get userid for:", config.TM.Username)
 	}
 	v.Set("follow", strconv.FormatInt(user.Id, 10))
 	twitterStream := client.PublicStreamFilter(v)
@@ -67,7 +84,7 @@ func Monitor() {
 		case anaconda.Tweet:
 			Log.Print(log.LogLevelTrace, "Receiving tweet:", tweet.Text)
 
-			go SendMessage(client, Log, message, item.(anaconda.Tweet).User.ScreenName)
+			go SendMessage(client, Log, config.TM.Message, item.(anaconda.Tweet).User.ScreenName)
 
 		default:
 			Log.Print(log.LogLevelError, "recived unknown type")
